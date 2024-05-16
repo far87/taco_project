59b58898cb124322b2f9d5dd282f2971ae9cd558
Raffaele Maisto <far1987@libero.it>
2024-05-16 18:51:10 +0200

    adds security test technology and makes security tests
    import spring security test technology(@WitMockUser) and make first security tests

--- src/test/java/com/taco/DesignTacoControllerTest.java
+++ src/test/java/com/taco/DesignTacoControllerTest.java
@@ -6,17 +6,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.ArrayList;
 
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
 import com.taco.domain.Ingredient;
 import com.taco.repository.IngredientRepository;
 import com.taco.web.DesignTacoController;
@@ -25,15 +31,29 @@ import com.taco.web.DesignTacoController;
 public class DesignTacoControllerTest {
 
 	@Autowired
+	private ApplicationContext applicationContext;
+	
+	@Autowired
 	private MockMvc mockMvc;
 	
 	@MockBean
 	private IngredientRepository ingredientRepository;
 	
-	@Test 
-	public void getDesignViewTest() throws Exception{
+	@Test
+	public void unauthorizedDesignViewTest() throws Exception{
+        UsernamePasswordAuthenticationToken principal =new UsernamePasswordAuthenticationToken("user", "password");
+        SecurityContextHolder.getContext().setAuthentication(principal);       
 		when(ingredientRepository.findAll()).thenReturn(new ArrayList<Ingredient>());
 		mockMvc.perform(get("/design"))
 		.andExpect(status().isUnauthorized());		
 	}
+	
+	@Test
+	@WithMockUser(username = "username",password = "password",roles = "USER")
+	public void AuthorizedDesignViewTest() throws Exception{
+		when(ingredientRepository.findAll()).thenReturn(new ArrayList<Ingredient>());
+		mockMvc.perform(get("/design"))
+		.andExpect(status().isOk());		
+	}
+
 }
